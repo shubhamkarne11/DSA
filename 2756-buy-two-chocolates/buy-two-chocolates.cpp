70a4b85eb2e0e6a471de8e35b30eb3087f2b0fdd
shubham karne <120401166+shubhamkarne11@users.noreply.github.com>
2025-03-19 21:00:42 +0530

    Time: 1 ms (30.09%) | Memory: 52.2 MB (30.56%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -3,10 +3,12 @@ public:
     int buyChoco(vector<int>& prices, int money) {
         int n = prices.size();
         sort(prices.begin(), prices.end());
-        if(money>=(prices[0]+prices[1])){
-            return {money-(prices[0]+prices[1])};
-        }else{
-            return money;
-        }
+        int rem = money - (prices[0] + prices[1]);
+        return (rem<0) ? money : rem;
+        // if(money>=(prices[0]+prices[1])){
+        //     return {money-(prices[0]+prices[1])};
+        // }else{
+        //     return money;
+        // }
     }
 };
