047cbe17ec6786e5f440c8efa00b7cb74c36271a
shubham karne <120401166+shubhamkarne11@users.noreply.github.com>
2025-03-23 09:52:41 +0530

    Time: 4 ms (20.57%) | Memory: 8.6 MB (3.55%) - LeetSync

--- 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.cpp
+++ 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     int maxContainers(int n, int w, int maxWeight) {
         int first = n*n;
-        int i;
-        for(i=first;i>=1;i--){
-            if(i*w<=maxWeight){
-                break;
+        int i=first;
+        while(i>=1){
+           if (i * w <= maxWeight) {
+                break;  // Stop when condition is met
+            } else {
+                i--;    // Decrement i if condition is not met
             }
         }
+        // for(i=first;i>=1;i--){
+        //     if(i*w<=maxWeight){
+        //         break;
+        //     }
+        // }
         return i;
     }
 };
