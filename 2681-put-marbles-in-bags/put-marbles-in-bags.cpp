c841f311f5c517c4de0a30dbbb98c78f75881775
shubham karne <120401166+shubhamkarne11@users.noreply.github.com>
2025-03-31 14:16:37 +0530

    Time: 33 ms (76.79%) | Memory: 64.1 MB (91.14%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
         int n = weights.size();
-        long m = n-1;
-        vector<int>pairsum(m,0);
+        int m = n-1;
+        vector<int>putmarbles(m,0);
+        for(int i=0;i<m;i++){
+            putmarbles[i]=weights[i]+weights[i+1];
+        }
+        sort(putmarbles.begin(),putmarbles.end());
         long minsum = 0;
         long maxsum = 0;
-        for(int i=0;i<m;i++){
-            pairsum[i]= weights[i]+weights[i+1];
-        }
-
-        sort(pairsum.begin(), pairsum.end());
         for(int i=0;i<k-1;i++){
-            minsum += pairsum[i];
-            maxsum += pairsum[m-1-i];
+            minsum+=putmarbles[i];
+            maxsum+=putmarbles[m-1-i];
         }
-
         return maxsum-minsum;
+
     }
 };
