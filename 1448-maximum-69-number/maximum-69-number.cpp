dfa07b4368641991a0eeab0326787ec632607512
shubham karne <120401166+shubhamkarne11@users.noreply.github.com>
2025-08-16 23:09:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (94.22%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     int maximum69Number (int num) {
         string num1 = to_string(num);
-        int ans = num;
-        int num2;
         for(int i=0;i<num1.size();i++){
-            string change = num1;
-            if(change[i]=='6'){
-                change[i]='9';
+            if(num1[i]=='6'){
+                num1[i]='9';
+                break;
             }
-            num2 = stoi(change);
-            ans = max(ans,num2);
-            change = num1;
         }
-        return ans;
+        return stoi(num1);
     }
 };
